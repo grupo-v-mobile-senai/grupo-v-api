c8a693b6e0601348c4ca2242c4011e6f3f63e0cb
ALLYSON FELIPPO BREDA DE MONTE CHRISTO <allyson_christo@estudante.sesisenai.org.br>
2024-06-12 21:10:34 -0300

    inserções da tabela categoria

--- banco-dados elecStock/tabela_usuario.sql
+++ banco-dados elecStock/tabela_usuario.sql
@@ -5,3 +5,17 @@ CREATE TABLE `usuario` (
   `senha` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- inserções padrão para iniciar a aplicação 
+INSERT INTO categoria
+(nome, cor, imagem)
+VALUES
+('Fones de ouvido','terceira','fone'),
+('CPUs','quarta','cpu'),
+('Mouses','primaria','mouse'),
+('Televisores','secundaria','televisor'),
+('Câmeras','terceira','camera'),
+('Carregadores','quarta','carregador'),
+('Relógios','primaria','relogio'),
+('Consoles','secundaria','console'),
+('Controles','terceira','controle');
